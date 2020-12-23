8da0e7959658aaf77b22711e3ab53b768ca6b82e
yusifk88 <amloyal4life@gmail.com>
2020-12-23 11:53:55 +0000

    removed DStv

--- resources/js/components/BillsComponent.vue
+++ resources/js/components/BillsComponent.vue
@@ -41,48 +41,44 @@
             </v-col>
 
 
+<!--            <v-col cols="12" sm="6">-->
+<!--                <v-hover v-slot="{ hover }">-->
 
+<!--                <v-card to="/buydstv">-->
+<!--                    <v-img-->
+<!--                    src="/img/dstv.jpg"-->
+<!--                    ></v-img>-->
+<!--                    <v-expand-transition>-->
+<!--                        <div-->
+<!--                            v-if="hover"-->
+<!--                            class="d-flex transition-fast-in-fast-out amber darken-2 v-card&#45;&#45;reveal display-3 white&#45;&#45;text"-->
+<!--                            style="height: 100%;"-->
+<!--                        >-->
+<!--                            <v-btn to="/buydstv" color="amber" depressed dark large>Buy Now!<v-icon>mdi-cart-plus</v-icon></v-btn>-->
+<!--                        </div>-->
+<!--                    </v-expand-transition>-->
+<!--                    <v-card-text style="position: relative;">-->
+<!--                        <v-btn-->
+<!--                            absolute-->
+<!--                            color="amber"-->
+<!--                            class="white&#45;&#45;text"-->
+<!--                            fab-->
+<!--                            large-->
+<!--                            right-->
+<!--                            top-->
+<!--                            v-if="!hover"-->
 
-
-
-            <v-col cols="12" sm="6">
-                <v-hover v-slot="{ hover }">
-
-                <v-card to="/buydstv">
-                    <v-img
-                    src="/img/dstv.jpg"
-                    ></v-img>
-                    <v-expand-transition>
-                        <div
-                            v-if="hover"
-                            class="d-flex transition-fast-in-fast-out amber darken-2 v-card--reveal display-3 white--text"
-                            style="height: 100%;"
-                        >
-                            <v-btn to="/buydstv" color="amber" depressed dark large>Buy Now!<v-icon>mdi-cart-plus</v-icon></v-btn>
-                        </div>
-                    </v-expand-transition>
-                    <v-card-text style="position: relative;">
-                        <v-btn
-                            absolute
-                            color="amber"
-                            class="white--text"
-                            fab
-                            large
-                            right
-                            top
-                            v-if="!hover"
-
-                        >
-                            <v-icon>mdi-cart</v-icon>
-                        </v-btn>
-                        <center>
-                        <h3>Pay TV Bill</h3>
-                        </center>
-                        Pay DStv, StartTime and Gotv Bills for a loved one.
-                    </v-card-text>
-                </v-card>
-                </v-hover>
-            </v-col>
+<!--                        >-->
+<!--                            <v-icon>mdi-cart</v-icon>-->
+<!--                        </v-btn>-->
+<!--                        <center>-->
+<!--                        <h3>Pay TV Bill</h3>-->
+<!--                        </center>-->
+<!--                        Pay DStv, StartTime and Gotv Bills for a loved one.-->
+<!--                    </v-card-text>-->
+<!--                </v-card>-->
+<!--                </v-hover>-->
+<!--            </v-col>-->
         </v-row>
 
 
